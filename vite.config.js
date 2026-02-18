478b44e2980f29ea7e1547048b9267ba803eec69
josedvega <davarela231@gmail.com>
2026-02-18 00:46:35 -0600

    Favorites route fixed

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        /* favorites: resolve(__dirname, "src/favorites/index.html"), */
+        favorites: resolve(__dirname, "src/favorites/index.html"),
         pokemon: resolve(__dirname, "src/pokemon/index.html"),
         header: resolve(__dirname, "src/public/partials/header.html"),
         footer: resolve(__dirname, "src/public/partials/footer.html"),
